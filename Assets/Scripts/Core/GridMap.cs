1b619bbe9a09ea0e68c2a34b84832c652f753104
GMihalkow <georgimihalkov@abv.bg>
2021-05-31 22:14:10 +0300

    fix: removed unused code & fixed Mover.GetPos()

--- Assets/Scripts/Core/GridMap.cs
+++ Assets/Scripts/Core/GridMap.cs
@@ -10,26 +10,12 @@ namespace LaserChess.Core
         [SerializeField] float xCellSize = 0.65f;
         [SerializeField] float yCellSize = 0.7f;
         
-        private float _minX;
-        private float _maxX;
-        private float _minY;
-        private float _maxY;
-
-        private void Awake()
-        {
-            this._minX = this.xCellSize / 2f;
-            this._maxX = (this.xCellSize * (float)cols) - (this.xCellSize / 2f);
-
-            this._minY = this.yCellSize / 2f;
-            this._maxY = (this.yCellSize * (float)rows) - (this.yCellSize / 2f);
-        }
-        
         public Vector2 GetPos(int row, int col)
         {
             if (row < 0 || row >= rows || col < 0 || col >= cols) throw new ArgumentOutOfRangeException("Coordinates are out of grid bounds!");
 
-            var x = (this.xCellSize * (float)col) - (this.xCellSize / 2f);
-            var y = (this.yCellSize * (float)row) - (this.yCellSize / 2f);
+            var x = (this.xCellSize * (float)row) - (this.xCellSize / 2f);
+            var y = (this.yCellSize * (float)col) - (this.yCellSize / 2f);
 
             return new Vector2(x, y);
         }
