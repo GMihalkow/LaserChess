b5d31429ba9acd1871379ecba475f4449fe3a5a3
GMihalkow <georgimihalkov@abv.bg>
2021-06-02 21:49:34 +0300

    fix: fixed Jumpship movement

--- Assets/Scripts/Pieces/Jumpship.cs
+++ Assets/Scripts/Pieces/Jumpship.cs
@@ -15,15 +15,15 @@ namespace LaserChess.Pieces
             for (int row = this._mover.CurrentRow - 2; row <= this._mover.CurrentRow + 2; row+=2)
             {
                 var isNotInBounds = row < 0 || row >= this._grid.Rows;
-                if (row == this._mover.CurrentRow || isNotInBounds || !this._grid.IsPosEmpty(row, this._mover.CurrentCol)) continue;
+                if (row == this._mover.CurrentRow || isNotInBounds) continue;
 
-                if (this._mover.CurrentCol - 1 >= 0)
+                if (this._mover.CurrentCol - 1 >= 0 && this._grid.IsPosEmpty(row, this._mover.CurrentCol - 1))
                 {
                     var marker = GameObject.Instantiate(this._markerPrefab, this._markersContainer.transform);
                     marker.GetComponent<Mover>().Move(row, this._mover.CurrentCol - 1);
                 }
 
-                if (this._mover.CurrentCol + 1 < this._grid.Cols)
+                if (this._mover.CurrentCol + 1 < this._grid.Cols && this._grid.IsPosEmpty(row, this._mover.CurrentCol + 1))
                 {
                     var marker = GameObject.Instantiate(this._markerPrefab, this._markersContainer.transform);
                     marker.GetComponent<Mover>().Move(row, this._mover.CurrentCol + 1);
@@ -33,15 +33,15 @@ namespace LaserChess.Pieces
             for (int col = this._mover.CurrentCol - 2; col <= this._mover.CurrentCol + 2; col+=2)
             {
                 var isNotInBounds = col < 0 || col >= this._grid.Cols;
-                if (col == this._mover.CurrentCol || isNotInBounds || !this._grid.IsPosEmpty(this._mover.CurrentRow, col)) continue;
+                if (col == this._mover.CurrentCol || isNotInBounds) continue;
 
-                if (this._mover.CurrentRow - 1 >= 0)
+                if (this._mover.CurrentRow - 1 >= 0 && this._grid.IsPosEmpty(this._mover.CurrentRow - 1, col))
                 {
                     var marker = GameObject.Instantiate(this._markerPrefab, this._markersContainer.transform);
                     marker.GetComponent<Mover>().Move(this._mover.CurrentRow - 1, col);
                 }
 
-                if (this._mover.CurrentRow + 1 < this._grid.Rows)
+                if (this._mover.CurrentRow + 1 < this._grid.Rows && this._grid.IsPosEmpty(this._mover.CurrentRow + 1, col))
                 {
                     var marker = GameObject.Instantiate(this._markerPrefab, this._markersContainer.transform);
                     marker.GetComponent<Mover>().Move(this._mover.CurrentRow + 1, col);
