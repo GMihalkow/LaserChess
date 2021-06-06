29b1b2d62e1d3b20ed1fd1491c20ad0b1c8348d6
GMihalkow <georgimihalkov@abv.bg>
2021-06-06 18:35:06 +0300

    fix: started clearing grid spot when piece dies and used PieceIdsEnum for piece id

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace LaserChess.Core
 {
     public class Health : MonoBehaviour
     {
+        [SerializeField] UnityEvent _onDeath;
         [SerializeField] float _hitPoints = 5f;
 
         private float _currentHitPoints;
@@ -19,6 +21,7 @@ namespace LaserChess.Core
 
             if (!Mathf.Approximately(this._currentHitPoints, 0)) return;
 
+            this._onDeath.Invoke();
             GameObject.Destroy(this.gameObject);
         }
     }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,11 +1,12 @@
 ﻿using LaserChess.Core;
+using LaserChess.Core.Enums;
 using UnityEngine;
 
 namespace LaserChess.Movement
 {
     public class Mover : MonoBehaviour
     {
-        [SerializeField] int _id;
+        [SerializeField] PiecesIdsEnum _id;
 
         private GridMap _grid;
         private int _currentCol = -1;
@@ -25,7 +26,7 @@ namespace LaserChess.Movement
             var prevCol = this._currentCol;
             var prevRow = this._currentRow;
 
-            this.transform.position = this._grid.GetPosWorldCoords(row, col, this._id, setId);
+            this.transform.position = this._grid.GetPosWorldCoords(row, col, (int)this._id, setId);
 
             this._currentCol = col;
             this._currentRow = row;
@@ -33,6 +34,14 @@ namespace LaserChess.Movement
             if ((prevCol == col && prevRow == row) || (prevRow < 0 || prevCol < 0) || this.CompareTag("PositionMarker")) return;
 
             this._grid.ClearPos(prevRow, prevCol);
+        }
+
+        /// <summary>
+        /// Called from the editor
+        /// </summary>
+        public void ClearPos()
+        {
+            this._grid.ClearPos(this._currentRow, this._currentCol);
         }
     }
 }
